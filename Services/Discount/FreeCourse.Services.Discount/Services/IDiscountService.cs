784f2e4409e8c7961e326f5f61ed1069d80da3a6
Barış Kocaman <bariskocaman5592@gmail.com>
2023-04-12 23:08:34 +0300

    discountAPI microservice project implementations started

---
+++ Services/Discount/FreeCourse.Services.Discount/Services/IDiscountService.cs
@@ -0,0 +1,16 @@
+﻿using FreeCourse.Shared.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<ResponseDto<List<Models.Discount>>> GetAll();
+        Task<ResponseDto<Models.Discount>> GetById(int id);
+        Task<ResponseDto<NoContent>> Save(Models.Discount discount);
+        Task<ResponseDto<NoContent>> Update(Models.Discount discount);
+        Task<ResponseDto<NoContent>> Delete(int id);
+        Task<ResponseDto<Models.Discount>> GetByCodeAndUserId(string code, string userId);
+    }
+}
