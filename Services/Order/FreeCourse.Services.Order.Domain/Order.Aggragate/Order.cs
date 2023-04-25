17cdec7caea610b46ab3d560a08bb44bc153cbbb
Barış Kocaman <bariskocaman5592@gmail.com>
2023-04-25 16:49:56 +0300

    OrderApi microservice Domain project commit

---
+++ Services/Order/FreeCourse.Services.Order.Domain/Order.Aggragate/Order.cs
@@ -0,0 +1,43 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.Order.Aggragate
+{
+    // EF Core Features
+    // --Owned Types
+    // --Shadow Property
+    // --Backing Field
+    // Bir Aggregate Root bir entity kullanıyorsa, o entity başka bir Aggrageta Root tarafından kullanılmamalıdır.
+    public class Order : Entity, IAggragateRoot
+    {
+        private readonly List<OrderItem> _orderItems;
+
+        public DateTime CreatedDate { get; private set; }
+        public Address Address { get; private set; }
+        public string BuyerId { get; private set; }
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+        public Order(Address address, string buyerId)
+        {
+            Address = address;
+            BuyerId = buyerId;
+            CreatedDate = DateTime.Now;
+            _orderItems = new List<OrderItem>();
+        }
+
+        public void AddOrderItem(string productId, string productName, Decimal price, string pictureUrl)
+        {
+            var existsProduct = _orderItems.Any(i => i.ProductId == productId);
+            if(!existsProduct)
+            {
+                var newOrderItem = new OrderItem(productId, productName, pictureUrl, price);
+                _orderItems.Add(newOrderItem);
+            }
+        }
+
+        public Decimal GetTotalPrice => _orderItems.Sum(i => i.Price);
+    }
+}
