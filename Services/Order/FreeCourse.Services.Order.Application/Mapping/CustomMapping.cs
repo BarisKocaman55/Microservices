173c22251d7c3c15e7e4c667f71e43b6c865c9c8
Barış Kocaman <bariskocaman5592@gmail.com>
2023-04-25 17:54:37 +0300

    dto objects and autoMapper implementations has been completed

---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Order.Application.Dtos;
+using FreeCourse.Services.Order.Domain.Order.Aggragate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Application.Mapping
+{
+    public class CustomMapping : Profile
+    {
+        public CustomMapping()
+        {
+            CreateMap<Domain.Order.Aggragate.Order, OrderDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<AddressDto, AddressDto>().ReverseMap();
+        }
+    }
+}
