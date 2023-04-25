55ade75253ff585809a81322f270554e73549f72
Barış Kocaman <bariskocaman5592@gmail.com>
2023-04-25 17:34:01 +0300

    OrderApi microservice orderDbContext has been implemented

---
+++ Services/Order/FreeCourse.Services.Order.Infrastructure/OrderDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Infrastructure
+{
+    public class OrderDbContext : DbContext
+    {
+        public const string DEFAULT_SCHEMA = "ordering";
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Domain.Order.Aggragate.Order> Orders { get; set; }
+        public DbSet<Domain.Order.Aggragate.OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.Order.Aggragate.Order>().ToTable("Orders", DEFAULT_SCHEMA);
+            modelBuilder.Entity<Domain.Order.Aggragate.OrderItem>().ToTable("Orders", DEFAULT_SCHEMA);
+
+            modelBuilder.Entity<Domain.Order.Aggragate.OrderItem>().Property(i => i.Price).HasColumnType("decimal(18, 2)");
+
+            // Domain içerisinde ORM araçları ile ilgili kod yazmıyoruz. Böylece sızıntı olmamış oluyor. Order içerisindeki Addresin owned olduğunu burada tanımlıyoruz.
+            modelBuilder.Entity<Domain.Order.Aggragate.Order>().OwnsOne(i => i.Address).WithOwner();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
