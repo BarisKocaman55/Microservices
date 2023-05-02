b607ffe5602a2d8573e3df0ea0c606fb3ec97049
Barış Kocaman <bariskocaman5592@gmail.com>
2023-05-02 22:25:56 +0300

    fakePaymentAPI implementations and identityServer for fakePayment has been completed

---
+++ Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentController.cs
@@ -0,0 +1,18 @@
+﻿using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentController : BaseController
+    {
+        [HttpPost]
+        public IActionResult RecievePayment()
+        {
+            return CreateActionResultInstance(ResponseDto<NoContentResult>.Success(200));
+        }
+    }
+}
